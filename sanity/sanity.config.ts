6cf1a4f8047eb3b8eee22965f2c19515feb3fab8
Oleksandr Sekker <oleksandr.sekker@gmail.com>
2025-12-04 15:05:13 +0200

    Update Next.js config for Sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,17 +1,15 @@
-import {defineConfig} from 'sanity/desk'
-import {structureTool} from 'sanity/desk'
+// sanity.config.ts
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'monaco-site',
-
   projectId: 'bzqr2t71',
   dataset: 'production',
-
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
