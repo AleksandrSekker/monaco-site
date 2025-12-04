173d7d2460b65bc694aa4b53e6b013b8de3044f8
Oleksandr Sekker <oleksandr.sekker@gmail.com>
2025-12-04 15:11:36 +0200

    Update Sanity config imports

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,15 +1,15 @@
 // sanity.config.ts
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {createConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
+export default createConfig({
   name: 'default',
   title: 'monaco-site',
   projectId: 'bzqr2t71',
   dataset: 'production',
-  plugins: [structureTool(), visionTool()],
+  plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
