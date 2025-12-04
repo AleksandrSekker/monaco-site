5f4f7bbc3d9f716e5849e95cd19c6443ef9def29
Oleksandr Sekker <oleksandr.sekker@gmail.com>
2025-12-04 19:02:55 +0200

    fix

--- src/lib/sanity.queries.ts
+++ src/lib/sanity.queries.ts
@@ -1,5 +1,5 @@
 import { groq } from 'next-sanity';
-import { client } from '@/sanity/lib/client';
+import { client } from '../sanity/client';
 
 export async function getPosts() {
   return client.fetch(groq`
---
+++ src/sanity/client.ts
@@ -0,0 +1,1 @@
+../../sanity/lib/client.ts
